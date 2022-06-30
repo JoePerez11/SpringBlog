0570b0139c5a8700eafa4f26e687e4aa30f7571b
JoePerez11 <perez.joseph.k@gmail.com>
2022-06-30 09:59:41 -0500

    First Spring commit

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HomeController {
+        @GetMapping("/hello")
+        @ResponseBody
+        public String hello() {
+            return "This is the landing page!";
+        }
+    }
+
